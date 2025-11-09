e048fffe9fb54c88294768c1892003fe16edeb8f
Takahiro Yano <speg03@gmail.com>
2025-11-09 17:48:33 +0900

    9.2.3 修正版のVPC作成: バックエンドのロックが管理されていない
    backendの設定を別ファイルに切り出し（migrate-state必要）
    terraform init -migrate-state -backend-config=./backend/backend.tfvars

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "speg03-sandbox-terraform-state-sample-bucket"
-    key    = "090_vpc/vpc.tfstate"
-    region = "ap-northeast-1"
-  }
+  backend "s3" {}
 }
