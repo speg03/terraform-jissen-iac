13a83b059f7c110a7c52eb8d760fb8452c245ce9
Takahiro Yano <speg03@gmail.com>
2025-11-11 20:57:20 +0900

    backendや変数の設定方法を整理
    * backend設定の別ファイルbackend/backend.tfvarsを削除
    * 使用しないdev環境向けの個別変数定義env/dev/tfvarsを削除
    * 使用していない空のvariables.tfを削除

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "speg03-sandbox-terraform-state-sample-bucket"
+    key          = "terraform-state-sample.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+  }
 }
