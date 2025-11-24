06d3052cb1811bd76c15f811db8f2ecaba440615
Takahiro Yano <speg03@gmail.com>
2025-11-24 14:43:26 +0900

    11.4.2 プライベートサブネットのルートテーブルの作成

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -26,3 +26,32 @@ resource "aws_route_table_association" "public_route_table_associations" {
   route_table_id = aws_route_table.public_route_tables[each.key].id
   subnet_id      = each.value.id
 }
+
+# プライベートサブネット用のルートテーブル
+resource "aws_route_table" "private_route_tables" {
+  for_each = aws_subnet.private_subnets
+
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name  = "${var.service_name}-${var.env}-${each.value.availability_zone}-private-route-table"
+    VpcId = aws_vpc.vpc.id
+    Scope = "private"
+  }
+}
+
+# プライベートサブネット用のデフォルトルート設定
+resource "aws_route" "private_default_route" {
+  for_each = aws_subnet.private_subnets
+
+  route_table_id         = aws_route_table.private_route_tables[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateways[each.value.availability_zone].id
+}
+
+# プライベートサブネットとプライベートサブネット用のルートテーブルのひも付け
+resource "aws_route_table_association" "private_route_table_associations" {
+  for_each = aws_subnet.private_subnets
+
+  route_table_id = aws_route_table.private_route_tables[each.key].id
+  subnet_id      = each.value.id
+}
