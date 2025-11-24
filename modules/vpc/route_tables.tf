51a47512f89487cf7d510ee1f1769a14f1e56c89
Takahiro Yano <speg03@gmail.com>
2025-11-24 14:32:05 +0900

    11.4.1 パブリックサブネットのルートテーブルの作成

---
+++ modules/vpc/route_tables.tf
@@ -0,0 +1,28 @@
+# パブリックサブネット用のルートテーブル
+resource "aws_route_table" "public_route_tables" {
+  for_each = aws_subnet.public_subnets
+
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name  = "${var.service_name}-${var.env}-${each.value.availability_zone}-public-route-table"
+    VpcId = aws_vpc.vpc.id
+    Scope = "public"
+  }
+}
+
+# パブリックサブネット用のデフォルトルート設定
+resource "aws_route" "public_default_route" {
+  for_each = aws_subnet.public_subnets
+
+  route_table_id         = aws_route_table.public_route_tables[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# パブリックサブネットとパブリックサブネット用のルートテーブルのひも付け
+resource "aws_route_table_association" "public_route_table_associations" {
+  for_each = aws_subnet.public_subnets
+
+  route_table_id = aws_route_table.public_route_tables[each.key].id
+  subnet_id      = each.value.id
+}
